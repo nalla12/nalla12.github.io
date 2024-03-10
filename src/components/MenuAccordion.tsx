7dbad68ca86df840bd091264ba412f854a96a050
nalla12 <alland12@hotmail.com>
2024-03-10 21:12:27 +0100

    Change title color onFocus

--- src/components/MenuAccordion.tsx
+++ src/components/MenuAccordion.tsx
@@ -1,6 +1,9 @@
 import {Accordion, AccordionItem} from '@nextui-org/react';
+import {useState} from 'react';
 
 export default function MenuAccordion() {
+    const [itemInFocus, setItemInFocus] = useState<undefined | number>();
+
     const defaultContent =
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
 
@@ -32,7 +35,6 @@ export default function MenuAccordion() {
             className='max-w-screen-md mx-auto'
             itemClasses={{
                 subtitle: 'ml-2 text-lg',
-                title: 'inline text-4xl text-amber-400',
                 titleWrapper: 'flex-row items-end'
             }}
             motionProps={{
@@ -77,6 +79,12 @@ export default function MenuAccordion() {
                     aria-label={item.title}
                     title={item.title}
                     subtitle={item.subtitle}
+                    onFocus={() => setItemInFocus(index)}
+                    classNames={{
+                        title: itemInFocus === index
+                            ? 'text-amber-400 inline text-4xl'
+                            : 'inline text-4xl'
+                    }}
                 >
                     {item.content}
                 </AccordionItem>
