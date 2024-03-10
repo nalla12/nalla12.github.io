bc5767bf7931889a7d151c0d216b43b611772213
nalla12 <alland12@hotmail.com>
2024-03-10 17:32:45 +0100

    Installed postcss and autoprefixer. Added config file postcss.config.cjs to make TailwindCSS work.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 // tailwind.config.js
-const { nextui } = require("@nextui-org/react");
+const { nextui } = require('@nextui-org/react');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        // ...
-        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}',
+        './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
         extend: {},
     },
-    darkMode: "class",
+    darkMode: 'class',
     plugins: [nextui()]
 }
