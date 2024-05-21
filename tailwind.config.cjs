467d9ec42603905d6e8c58210c2eca4fc6e5f648
nalla12 <alland12@hotmail.com>
2024-05-21 13:20:50 +0200

    Add .mdx to make sure Tailwind scans MDX files.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ const { nextui } = require('@nextui-org/react');
 module.exports = {
     content: [
         './index.html',
-        './src/**/*.{js,ts,jsx,tsx}',
+        './src/**/*.{js,ts,jsx,tsx,mdx}',
         './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
